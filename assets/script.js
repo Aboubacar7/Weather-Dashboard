ec09d60d01dede236a792cc03288954708be6dc8
Aboubacar7 <abdrago17@yahoo.com>
2023-02-02 22:48:50 -0800

    adding styling

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,7 @@ function getCity(event) {
     event.preventDefault()
     var city = cityInput.value
 
+
     getCurrentForecast(city)
 }
 
@@ -22,63 +23,91 @@ function getCurrentForecast(city) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=imperial`)
         .then(response => response.json())
         .then(currentData => {
-           
-           console.log(currentData)
-           localStorage.setItem("currentData.name", JSON.stringify(currentData.name))
-           var cityHistory = document.querySelector(".city-History")
-           cityHistory.textContent = currentData.name
-           console.log(currentData.name)
+
+            console.log(currentData)
+            let storedCities = localStorage.getItem("city")
+                ? JSON.parse(localStorage.getItem("city"))
+                : [];
+
+            storedCities.push(currentData.name);
+            localStorage.setItem("city", JSON.stringify(storedCities));
+
 
             var lat = currentData.coord.lat
             var lon = currentData.coord.lon
             getFiveDayForecast(lat, lon)
-            
+
+            var currentD = document.querySelector(".current-D")
+            currentD.setAttribute('class', ' card ')
+
             var currentCity = document.querySelector('.current-city')
+            currentCity.setAttribute("style", " font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;")
+
             currentCity.textContent = currentData.name + dayjs.format("     (MM/DD/YYYY) 🔆")
-            
+
 
             var currentTemp = document.querySelector('.current-temp')
-            currentTemp.textContent = currentData.main.temp
+            currentTemp.textContent = "Temp: " + currentData.main.temp + " F"
+
+            var currentWind = document.querySelector('.current-wind')
+            currentWind.textContent = "Wind: " + currentData.wind.speed + " MHP"
 
             var currentTemp = document.querySelector('.current-humidity')
-            currentTemp.textContent = currentData.main.humidity+ "%"
+            currentTemp.textContent = "Temps: " + currentData.main.humidity + "%"
         })
         .catch(err => console.log(err))
-        getStorage()
 }
 
-function getStorage() {
+function getCityHistory() {
+    var saveCity = JSON.parse(localStorage.getItem("city"))
     for (var i = 0; i < 8; i++) {
-      var task = localStorage.getItem(i)
-      cityHistory.textContent(task)
+
+        var button = document.createElement("button")
+        button.innerText = saveCity[i]
+        var cityHistory = document.querySelector('.city-History')
+
+        cityHistory.append(button)
     }
-  }
+}
+getCityHistory()
+
 
 function getFiveDayForecast(lat, lon) {
-    
+
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`)
         .then(response => response.json())
         .then(fiveData => {
             console.log(fiveData)
 
+            var FiveDayForecastTitle = document.querySelector(".FivedayTitle")
+            fiveDayEl.innerHTML = ""
+
+            var fiveDayF = document.createElement('p')
+            fiveDayF.textContent = ""
+            fiveDayF.textContent = " 5-Day Forecast:  "
+            FiveDayForecastTitle.append(fiveDayF)
+            fiveDayF.setAttribute ("style", 'font-family: Impact, Haettenschweiler, Arial Narrow Bold, sans-serif')
+        
+
             for (var i = 0; i < 5; i++) {
+
                 var card = document.createElement('div')
                 card.setAttribute('class', 'card')
                 card.classList.add('p-10')
                 fiveDayEl.append(card)
-    
+
 
                 var fiveDayDate = document.createElement('h4')
                 fiveDayDate.textContent = dayjs.add(i + 1, 'days').format("MM/DD/YYYY")
                 card.append(fiveDayDate)
 
                 var fiveDayImo = document.createElement('h5')
-                fiveDayImo.textContent = " 🔆 " 
+                fiveDayImo.textContent = " 🔆 "
                 card.append(fiveDayImo)
 
 
                 var fiveDayTemp = document.createElement('p')
-                fiveDayTemp.textContent = "Temp: " + fiveData.list[i * 8].main.temp+ " F"
+                fiveDayTemp.textContent = "Temp: " + fiveData.list[i * 8].main.temp + " F"
                 card.append(fiveDayTemp)
 
                 var fiveDayWind = document.createElement('p')
